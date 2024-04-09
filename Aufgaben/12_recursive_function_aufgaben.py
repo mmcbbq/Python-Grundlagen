8166c991fad0883e823e69da8a537ed3cdddb06d
mmcbbq <m.martinez@bbq.de>
2024-04-09 11:03:58 +0200

    neuer name

--- Aufgaben/12_recursive_function_aufgaben.py
+++ Aufgaben/12_recursive_function_aufgaben.py
@@ -1,5 +1,63 @@
-
 # Schreibe eine rekursive Funktion power(x, n), die die Potenz x hoch n berechnet.
+
+def power(x, n):
+	if n == 1:
+		return x
+	else:
+		return x * power(x, n - 1)
+
+
+print(power(3, 3))
+
+# return 2 ** power(3, 1)
+#
+# return 3 ** power(1, 2)
+#
+# return 1 ** power(2,0)
+# return 1
+#
+# return 1
+# return 3
+# return 2 3 ->
+
 # Schreibe eine rekursive Funktion reverse_string(s), die eine Zeichenkette s umkehrt.
+
+def reverse_string(s):
+	if s == "":
+		return s
+	else:
+		return s[-1] + reverse_string(s[:-1])
+
+
+print(reverse_string("test"))
+
+
+
 # Schreibe eine rekursive Funktion is_palindrome(s), die überprüft, ob eine Zeichenkette s ein Palindrom ist.
+
+
+def is_palindrome(string, index=0):
+    if index <= len(string) // 2:
+        return string[index].lower() == string[len(string) - 1 - index].lower() and is_palindrome(string, index + 1)
+    else:
+        return True
+
+
+print(is_palindrome('Anna'))
+
+
+
 # Schreibe eine rekursive Funktion count_consonants(s), die die Anzahl der Konsonanten in einer Zeichenkette s zählt.
+
+def count_consonants(s):
+	vokale = "aeiou"
+	s = s.lower()
+	if s == "":
+		return 0
+	else:
+
+		if s[0] not in vokale:
+
+			return 1 + count_consonants(s[1:])
+		else:
+			return count_consonants(s[1:])
--- Lösungen/12_recursive_function_aufgaben_loesungen.py
+++ Lösungen/12_recursive_function_aufgaben_loesungen.py
@@ -1,11 +1,37 @@
-
 # Schreibe eine rekursive Funktion power(x, n), die die Potenz x hoch n berechnet.
+
+def power(x, n):
+	if n == 1:
+		return x
+	return x * power(x, n - 1)
+
+
+# power(3,3) return x * power(3,2)
+# power(3,2) return x * power(3,1)
+# power(3,1) return x
+
+# power(3,2) return x * x
+# power(3,3) return x * x * x
+print(power(2, 8))
+
+
 # Schreibe eine rekursive Funktion reverse_string(s), die eine Zeichenkette s umkehrt.
+
+def reverse_string(s):
+	if len(s) == 1:
+		return s
+	else:
+		return reverse_string(s[1:]) + s[0]
+
+
+my_string = ""
+print(my_string[1:])
+# print(reverse_string(my_string))
+
+
 # Schreibe eine rekursive Funktion is_palindrome(s), die überprüft, ob eine Zeichenkette s ein Palindrom ist.
 def palindrom(wort):
-	pali = True
 	if len(wort) <= 1:
-		print(f'pali{pali}')
 		return True
 	else:
 		if wort[0] == wort[-1]:
@@ -14,7 +40,20 @@ def palindrom(wort):
 		else:
 			return False
 
-print("qotto"[1:-1])
 
-print(palindrom("otto"))
+print('ist palindrome',palindrom("ot1ato"))
+
+
 # Schreibe eine rekursive Funktion count_consonants(s), die die Anzahl der Konsonanten in einer Zeichenkette s zählt.
+
+def count_consonants(s):
+	if len(s) == 0:
+		return 0
+
+	elif s[0] in 'aeiou':
+		return 0 + count_consonants(s[1:])
+	else:
+		return 1 + count_consonants(s[1:])
+
+
+print(count_consonants("abcdef"))

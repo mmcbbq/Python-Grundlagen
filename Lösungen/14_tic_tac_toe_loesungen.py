8ad45e10eb55211dc692b8a9caf26a232571eef5
mmcbbq <m.martinez@bbq.de>
2024-10-31 15:31:50 +0100

    add kassenzetttel aufgabe

--- Lösungen/14_tic_tac_toe_loesungen.py
+++ Lösungen/14_tic_tac_toe_loesungen.py
@@ -10,90 +10,87 @@
 #       |     |
 spielfeld = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
 spieler = [{'name': input(('name spieler 1')), 'zeichen': input("zeichen spieler 1")},
-		   {'name': input(('name spieler 2')), 'zeichen': input("zeichen spieler 2")}]
-
-
-
-
+           {'name': input(('name spieler 2')), 'zeichen': input("zeichen spieler 2")}]
 aktueller_spieler = spieler[1]
-def show_game(field):
-	print(f'y3{field[2]}')
-	print(f'y2{field[1]}')
-	print(f'y1{field[0]}')
-	print(f'\tx1 \t x2    x3')
 
 
-def ask_move():
-	x = int(input('x 1 2 3 ?'))
-	y = int(input('y 1 2 3 ?'))
-	return x - 1, y - 1
+def show_game(field: list) -> None:
+    print(f'y3{field[2]}')
+    print(f'y2{field[1]}')
+    print(f'y1{field[0]}')
+    print(f'\tx1 \t x2    x3')
 
 
-def check_move(move, feld):  # -> {2,1}
-	if feld[move[1]][move[0]] == "-":
-		return True
-	else:
-		return False
+def ask_move() -> tuple:
+    x = int(input('x 1 2 3 ?'))
+    y = int(input('y 1 2 3 ?'))
+    return x - 1, y - 1
 
 
-def set_move(feld, move, zeichen):
-	feld[move[1]][move[0]] = zeichen
+def check_move(move: tuple, feld: list) -> bool:  # -> {2,1}
+    if feld[move[1]][move[0]] == "-":
+        return True
+    else:
+        return False
 
 
-def sieg_w(feld):
-	for inner_list in feld:
-		if inner_list[0] == inner_list[1] and inner_list[0] == inner_list[2] and inner_list[0] != '-':
-			return True
-	return False
+def set_move(feld: list, move: tuple, zeichen: str) -> None:
+    feld[move[1]][move[0]] = zeichen
 
 
-def sieg_s(feld):
-	for x in range(3):
-		if feld[0][x] == feld[1][x] and feld[0][x] == feld[2][x] and feld[0][x] != "-":
-			return True
-	return False
+def sieg_w(feld: list) -> bool:
+    for inner_list in feld:
+        if inner_list[0] == inner_list[1] and inner_list[0] == inner_list[2] and inner_list[0] != '-':
+            return True
+    return False
 
 
-def sieg_dlr(feld):
-	if feld[0][0] == feld[1][1] and feld[0][0] == feld[2][2] and feld[0][0] != "-":
-		return True
-	return False
+def sieg_s(feld: list) -> bool:
+    for x in range(3):
+        if feld[0][x] == feld[1][x] and feld[0][x] == feld[2][x] and feld[0][x] != "-":
+            return True
+    return False
 
 
-def sieg_drl(feld):
-	if feld[0][2] == feld[1][1] and feld[0][2] == feld[2][0] and feld[1][1] != "-":
-		return True
-	return False
+def sieg_dlr(feld: list) -> bool:
+    if feld[0][0] == feld[1][1] and feld[0][0] == feld[2][2] and feld[0][0] != "-":
+        return True
+    return False
 
 
-def sieg(feld):
-	if sieg_w(feld) or sieg_s(feld) or sieg_dlr(feld) or sieg_drl(feld):
-		return True
-	else:
-		return False
+def sieg_drl(feld: list) -> bool:
+    if feld[0][2] == feld[1][1] and feld[0][2] == feld[2][0] and feld[1][1] != "-":
+        return True
+    return False
 
 
-def switch_player(player_now,player):
-	if player_now == player[0]:
-		return player[1]
-	else:
-		return player[0]
+def sieg(feld: list) -> bool:
+    if sieg_w(feld) or sieg_s(feld) or sieg_dlr(feld) or sieg_drl(feld):
+        return True
+    else:
+        return False
+
+
+def switch_player(player_now: dict, player: dict) -> dict:
+    if player_now == player[0]:
+        return player[1]
+    else:
+        return player[0]
 
 
 gewonnen = False
 show_game(spielfeld)
 while not gewonnen:
-	print(f'Spieler {aktueller_spieler["name"]} ist am Zug')
-	move = ask_move()
-	while not check_move(move, spielfeld):
-		print("Das geht nicht")
-		move = ask_move()
+    print(f'Spieler {aktueller_spieler["name"]} ist am Zug')
+    move = ask_move()
+    while not check_move(move, spielfeld):
+        print("Das geht nicht")
+        move = ask_move()
 
-	set_move(spielfeld, move, aktueller_spieler['zeichen'])
-	show_game(spielfeld)
-	gewonnen = sieg(spielfeld)
-	aktueller_spieler = switch_player(aktueller_spieler, spieler)
-
+    set_move(spielfeld, move, aktueller_spieler['zeichen'])
+    show_game(spielfeld)
+    gewonnen = sieg(spielfeld)
+    aktueller_spieler = switch_player(aktueller_spieler, spieler)
 
 aktueller_spieler = switch_player(aktueller_spieler, spieler)
 print(f'{aktueller_spieler["name"]} hat gewonnen')
--- show.py
+++
@@ -1,7 +0,0 @@
-liste = [1, 4, 7, 8, 12]
-search = 4
-index = len(liste) // 2
-start = 0
-end = len(liste)
-while liste[start:end][index] != search:
-    pass

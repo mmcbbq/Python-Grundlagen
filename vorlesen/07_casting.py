b1398b630c96a59d32dbb709dd5ed70df043a127
mmcbbq <m.martinez@bbq.de>
2024-11-21 14:08:02 +0100

    add type check

--- vorlesen/07_casting.py
+++ vorlesen/07_casting.py
@@ -1,10 +1,8 @@
-
 # In Python können wir eine Variable definieren und einen Wert zuweisen.
 # Der Datentype der Variable bestimmt Python "Duck Typing"
 #
 my_var = "Hello World"
 print(type(my_var))
-
 my_var = 15
 print(type(my_var))
 my_var = 15.5
@@ -18,9 +16,8 @@ print()
 print()
 print()
 
-
 zahl = "15"
-print('String: ',type(zahl), zahl)
+print('String: ', type(zahl), zahl)
 print(zahl * 3)
 zahl = int(zahl)
 print('Int:', type(zahl), zahl)
@@ -35,3 +32,15 @@ zahl = -10.9
 zahl = int(zahl)
 print(zahl)
 
+# ------type check----
+
+
+int_var = 12
+str_var = 'test'
+float_var = 12.334
+bool_var = True
+
+print(type(int_var) is int)
+print(type(str_var) is str)
+print(type(float_var) is float)
+print(type(bool_var) is bool)
